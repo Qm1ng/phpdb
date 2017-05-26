36a7d3741f943e7953334cd65de7ddaa0bb65fb7
Ming <qswag1957@gmail.com>
2017-05-27 02:24:47 +0800

    no message

---
+++ dbconnect.php
@@ -0,0 +1,11 @@
+<?php
+
+$hostname = "localhost";
+$user = "root";
+$password = "cccamp";
+$dbname = "cccamp";
+
+$link = @mysqli_connect($hostname, $user, $password, $dbname);
+mysqli_query($link, 'SET NAMES UTF-8');
+
+?>
